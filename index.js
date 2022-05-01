ffd85710e0eaa71764800aebedd19e503acf438b
ajphnx <ajph03nix@gmail.com>
2022-05-01 16:24:13 -0400

    finished and working

--- index.js
+++ index.js
@@ -1,126 +1,95 @@
 const fsKey='fsq3MbNrkxgPjUXYZohFDRG4bCQ4GTSYOUHggy/Fd267ieA='
-const menuList = ['coffee','restaurant','hotel','market']
-
+const menuList = ['default','coffee','restaurant','hotel','market']
+let menuChoice = menuList[2]
 const testLat = 35.8781704
 const testLong = -78.6565657
-
-//let actLat = null//testLat
-//let actLong = null//testLong
-/*Coors Issues
-`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`
-
-
-let data = await response.text();
-  let parsedData = JSON.parse(data);
-*/
-
-/************ 
- * +Get user location
- * ************/
-/*
- async function getCoords(){
-    return await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-        let pos = await 
-    })
-   // return [pos.coords.latitude, pos.coords.longitude]
-}
-
-
-
- async function getUserLocation(){
-
-    await navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude} = position.coords;
-        // actLat = position.coords.latitude
-        // actLong = position.coords.longitude
+const searchLimit = 5
+//iconSize =[20,20]
+let coords = []//[testLat,testLong]
+var businessData
+let myMap
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  })
+ 
+const dropDown = document.getElementById('business')
+dropDown.addEventListener("change",(e)=>{
+    menuChoice = e.target.value
+    
+    /************ 
+    * +Fetch Business Info
+    * ************/
+    async function getQuery(busQuery){
+        let lat = coords[0]
+            lat = lat.toFixed(2)
+        let long = coords[1]
+            long = long.toFixed(2)
+        let corsFix = 'https://cors-anywhere.herokuapp.com/'
+            console.log(`pre fetch: ${lat} , ${long}`)
+        const options = {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                Authorization: fsKey
+            }}
+            
+        const response = await fetch(`${corsFix}https://api.foursquare.com/v3/places/search?query=${busQuery}&ll=${lat}%2C${long}&limit=5&radius=500`, options)
         
-        // Show a map centered at latitude / longitude.
-      });
-    //return position.coords.latitude 
-    }
-*/
-async function getUserLocation(){
+        const data = await response.text()
+        let parseData = JSON.parse(data)
+        let businesses = parseData.results
+        businesses.forEach(business=>{
+            console.log(`post fetch: ${lat} , ${long}`)
 
-    await navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude} = position.coords;
-        // actLat = position.coords.latitude
-        // actLong = position.coords.longitude
-        
-        // Show a map centered at latitude / longitude.
-      });
-    //return position.coords.latitude 
+            console.log(`${business.name}:
+            ${business.geocodes.main.latitude},
+            ${business.geocodes.main.longitude}`)
+            let busLatLong=[business.geocodes.main.latitude,business.geocodes.main.longitude]
+            let pin = L.marker(busLatLong)
+            pin.addTo(myMap).bindPopup(`<p1><b>${business.name}</b></p1>`).openPopup()
+        })
+            return businesses  
     }
-
- /************ 
- * +Fetch Business  Query
- * ************/
-async function getQuery(busQuery){
-    const options = {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          Authorization: 'fsq3MbNrkxgPjUXYZohFDRG4bCQ4GTSYOUHggy/Fd267ieA='
-        }
-      };
-      
-    const response = fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?query=${busQuery}&ll=35.87%2C-78.65&radius=10&limit=10`, options)
+     businessData = getQuery(menuChoice)
+    //  generateMap()
+    .then (()=>{console.log(businessData)})
     
-    const data = await response
-    let parseData = data
-     /*let businesses = await parseData.results
-        console.log(businesses.results)
-    
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-        // console.log(JSON.stringify(response))
-    */
-   console.log(parseData)
-    }
-/************ 
- * +Generate List to select from
+ })
 
- * ************/
-function createMenu(){
-    document.createElementbvId('ul')
-    menuList.forEach(menuElement,()=>{
-      let item = document.createElementBy('li')
-      item.textContent = menuElement
+const generateMap = async () =>{
+    coords = await navigator.geolocation.getCurrentPosition(position => {
+        const {latitude, longitude} = position.coords;
+        coords = [position.coords.latitude,position.coords.longitude]
+        console.log(coords)
+        /************ 
+         * +Create Map
+        *************/ 
+        myMap = L.map('map',{
+            center: coords,
+            zoom:12,
+        })
+        //***** add openstreetmap tiles
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            minZoom: '15',
+        }).addTo(myMap)
+        //*****  create and main add geolocation marker
+        const marker = L.marker(coords, {icon: greenIcon})
+        marker.addTo(myMap).bindPopup('<p1><b>I am Here</b></p1>').openPopup()
     })
 }
 
-/************ 
- * +Create Map
-* ************/ 
-// function createMap(){
-//if(actLat != null && actLong!= null){
-    const myMap = L.map('map',{
-        center: [testLat,testLong],
-        zoom:12,
-    })
-
-// add openstreetmap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        minZoom: '15',
-    }).addTo(myMap);
-
-    // create and main add geolocation marker
-    const marker = L.marker([testLat, testLong])
-    marker.addTo(myMap).bindPopup('<p1><b>I am Here</b></p1>').openPopup()
-//}
-// }
-function main(){
-    // createMenu()
-    //getUserLocation()
-    console.log(getCoords())
-    console.log(`Menu Selection: ${menuList[0]}`)
-    // console.log (actLat)
-    // console.log (actLong)
-    // getQuery(`${menuList[0]}`)
-    // createMap()
+async function main(){
     
-
+    generateMap()
+    //coords = generateMap()
+    console.log(`Initial Menu Selection: null`)
+    console.log(typeof coords)
 }
+
 main()
